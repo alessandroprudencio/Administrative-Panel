b6d4bee226c72c0710a259a6515a4cc241ec4624
Alessandro Prudencio <alessandroconectado@gmail.com>
2018-11-07 19:25:08 -0200

    Estrutura do template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header/>
+		<Menu/>
+		<Content/>
+		<Footer/>
 	</div>
 </template>
 
 <script>
+import  Header  from "@/components/template/Header";
+import  Content  from "@/components/template/Content";
+import  Footer  from "@/components/template/Footer";
+import  Menu  from "@/components/template/Menu";
+
 export default {
 	name: "App",
+	components:{ Header, Content, Footer, Menu }
 }
 </script>
 
 <style>
+	*{
+		font-family:'Lato','sans-serif ';
+	}
+	body{
+		margin:0;
+	}
+	#app {
+		-webkit-font-smoothing:antialised;
+		-moz-osx-font-smoothing:grayscale;
 
+		height: 100vh;
+
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns:300px 1fr;
+
+		grid-template-areas: 
+		"header header"
+		"menu content"
+		"menu footer"
+	}
 </style>
---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">
+        <span> Alessandro Prudencio</span>
+    </footer>
+</template>
+
+<script>
+export default {    
+    name: "Footer"
+}
+</script>
+
+<style>
+    .footer{
+        grid-area: footer;
+        background-color: #DDD;
+        color:#333;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
